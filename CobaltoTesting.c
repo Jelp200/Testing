3d5f8bcc47c911c314ac88d368e1fbdab596650a
Jelp200 <jorge.pena0516@gmail.com>
2022-02-07 01:17:30 -0600

    :c: [ADD] Programm

---
+++ CobaltoTesting.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    char opc;
+
+    puts("\n\n\t***************************");
+    puts("\n\t***  COBALTO   TESTING  ***");
+    puts("\n\t***************************\n\n");
+
+    puts("A. Inicio\n");
+    puts("B. Clases\n");
+    puts("C. Comunicacion\n");
+    p
+    scanf("%c", &opc);
+
+    do{
+        switch(opc){
+            case 'A':
+                break;
+        }
+    }while(opc != 'S');
+    return 0;
+}
